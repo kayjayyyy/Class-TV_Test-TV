e15a8448d4bd81a71883c8d80dfaab8bdbb44ace
unknown <templanzatintin@gmail.com>
2023-05-18 21:45:40 +0800

    class TV

--- ClassTV.py
+++ ClassTV.py
@@ -1,3 +1,11 @@
 # Templanza, Kristine Joy F.
 # BSCPE 1-4
 # Asignnment no. 6 - Class TV_Test TV
+
+class TV:
+    def __init__(self, channel, volume, power):
+        # Parameretized constructor
+        self.getchannel = channel
+        self.getvolume = volume
+        self.TVpower = power
+        
--- TestTV.py
+++ TestTV.py
@@ -1,3 +1,9 @@
 # Templanza, Kristine Joy F.
 # BSCPE 1-4
 # Asignnment no. 6 - Class TV_Test TV
+
+# Greeting and border line
+print("")
+print("\033[35m※ \033[0m" * 40)
+print("")
+print("\033[45m ♥ Welcome! ♥ \033[0m".center(85))
