3a358ad2caf6d4f96fe13651e08ad0f43ed9702a
unknown <templanzatintin@gmail.com>
2023-05-19 23:48:58 +0800

    adding banner and design

--- TestTV.py
+++ TestTV.py
@@ -14,3 +14,11 @@ tv1 = TV
 tv2 = TV
 
 def TestTV():
+    
+    print("\n")
+    print("\033[1mT E L E V I S I O N  1\033[0m".center(85))
+    print("")
+    print("\033[31m-\033[0m" * 80)
+    print("\033[1;3;32mLoading..........\033[0m".center(90))
+    print("\033[31m-\033[0m" * 80)
+    
