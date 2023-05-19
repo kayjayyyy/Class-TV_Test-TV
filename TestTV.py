0c9e6abd6158ad90809d51e7f1418ff77134abce
unknown <templanzatintin@gmail.com>
2023-05-20 00:03:02 +0800

    pseudocode

--- TestTV.py
+++ TestTV.py
@@ -10,11 +10,13 @@ print("\033[45m ♥ Welcome! ♥ \033[0m".center(85))
 
 from ClassTV import TV
 
+# Assign tv1 and tv2
 tv1 = TV
 tv2 = TV
 
 def TestTV():
     
+    # Television 1 title and loading banner
     print("\n")
     print("\033[1mT E L E V I S I O N  1\033[0m".center(85))
     print("")
@@ -22,12 +24,16 @@ def TestTV():
     print("\033[1;3;32mLoading..........\033[0m".center(90))
     print("\033[31m-\033[0m" * 80)
     
+    # Assign value for tv1
     tv1 = TV(30, 3, True)
+    
+    # Print the television 1 outcome
     print("\n\033[36;1mtv1's channel is", tv1.getChannel() , "and volume level is", tv1.getVolume(), "\033[0m")
         
     print("\n")
     print("⨉ " * 40)
     
+    # Television 2 title and loading banner
     print("\n")
     print("\033[1mT E L E V I S I O N  2\033[0m".center(85))
     print("")
@@ -35,7 +41,10 @@ def TestTV():
     print("\033[1;3;32mLoading..........\033[0m".center(90))
     print("\033[31m-\033[0m" * 80)
     
+    # Assign value for tv2
     tv2 = TV(3, 2, True)
+    
+    # Print the television 2 outcome
     print("\n\033[36;1mtv2's channel is", tv2.getChannel() , "and volume level is", tv2.getVolume(), "\033[0m")
     print("")
     
