250cf75a3be489c859fa8c2118a1268c1c9c27de
unknown <templanzatintin@gmail.com>
2023-05-19 23:47:57 +0800

    define test tv and assign tv1 and tv2

--- TestTV.py
+++ TestTV.py
@@ -10,15 +10,7 @@ print("\033[45m ♥ Welcome! ♥ \033[0m".center(85))
 
 from ClassTV import TV
 
+tv1 = TV
+tv2 = TV
+
 def TestTV():
-    
-    print("")
-    print("-" * 70)
-    print("\033[1;3;32mProcessing..........\033[0m".center(80))
-    print("-" * 70)
-    
-    tv1 = TV(30, 3, True)
-    print("tv1's channel is", tv1.getChannel, "and volume level is", tv1.getVolume)
-    
-TestTV()
-    
